28d826dc01b69d03047e5a4ff73adbd31d7461de
F <saw@Saws-Air.gateway.2wire.net>
2016-07-31 00:01:36 +0800

    Simple Templating
    Added add.html, index.html
    
    knowns: render_template()

--- simple_app.py
+++ simple_app.py
@@ -1,17 +1,22 @@
 from flask import Flask
 # from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/<name>')
-def index(name="flaskaline"):
+def index(name="Flaskaline"):
     # name = request.args.get('name', name)
-    return 'Hello, {}!'.format(name)
+    return render_template("index.html", name=name)
 
 @app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-    return '{} + {} = {}'.format(num1, num2, num1+num2)
+    context = {'num1': num1, 'num2': num2}
+    return render_template("add.html", **context)
 
 app.run(debug=True)
