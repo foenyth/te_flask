649c7c3e57886efb84dd3921e143f440452d68d0
F <saw@Saws-Air.gateway.2wire.net>
2016-07-30 22:10:17 +0800

    Clean URL Arguments
    Math in the URL. Convert str into int.
    
    knowns: int:

--- simple_app.py
+++ simple_app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+# from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'Hello, World!'
+@app.route('/<name>')
+def index(name="flaskaline"):
+    # name = request.args.get('name', name)
+    return 'Hello, {}!'.format(name)
+
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    return '{} + {} = {}'.format(num1, num2, num1+num2)
 
 app.run(debug=True)
